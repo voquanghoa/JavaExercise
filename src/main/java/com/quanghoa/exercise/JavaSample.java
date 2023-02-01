7be89ee64fff1abe0c5219423da10876941dc5d3
Vo Quang Hoa <hvquang@openwt.com>
2023-02-01 11:24:05 +0700

    Unndo

--- src/main/java/com/quanghoa/exercise/JavaSample.java
+++ src/main/java/com/quanghoa/exercise/JavaSample.java
@@ -3,9 +3,6 @@ package com.quanghoa.exercise;
 public class JavaSample {
 
     public int sum(final int a, final int b) {
-        if (a == 100) {
-            return 0;
-        }
         return a + b;
     }
 }
